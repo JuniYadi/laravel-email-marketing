63c3005e0e865aa629bf6352dbec02a629d43b12
JuniYadi <juniyadi94@gmail.com>
2026-03-12 11:07:14 +0700

    test(home): cover redirect screen and unsafe HOME_REDIRECT values

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,5 +3,35 @@
 test('returns a successful response', function () {
     $response = $this->get(route('home'));
 
-    $response->assertOk();
+    $response
+        ->assertOk()
+        ->assertSeeText('Self-Hosted Email Marketing Platform')
+        ->assertDontSeeText('Redirect in progress');
+});
+
+test('home route renders redirect screen when home redirect is configured', function () {
+    config()->set('app.home_redirect', '/dashboard');
+
+    $response = $this->get(route('home'));
+
+    $response
+        ->assertOk()
+        ->assertSeeText('Redirect in progress')
+        ->assertSeeText('You will be redirected in 5 seconds to:')
+        ->assertSee('/dashboard', false)
+        ->assertSee('http-equiv="refresh"', false)
+        ->assertSee('content="5;url=/dashboard"', false)
+        ->assertSee(route('login'), false);
+});
+
+test('home route ignores unsafe home redirect values', function () {
+    config()->set('app.home_redirect', 'javascript:alert(1)');
+
+    $response = $this->get(route('home'));
+
+    $response
+        ->assertOk()
+        ->assertSeeText('Self-Hosted Email Marketing Platform')
+        ->assertDontSeeText('Redirect in progress')
+        ->assertDontSee('http-equiv="refresh"', false);
 });
